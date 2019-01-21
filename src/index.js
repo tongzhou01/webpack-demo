43d5bf278deaead1249e590ecad773024a9f1a84
tongzhou <568309196@qq.com>
2019-01-21 14:05:05 +0800

    图片资源管理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import image from './image.jpg';
 
 function component() {
-    var element = document.createElement('div')
+    var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' - ');
     element.classList.add('hello');
-    
+
+    // 将图像添加到我们现有的 div
+    var myIcon = new Image();
+    myIcon.src = image;
+
+    element.appendChild(myIcon);
+
     return element;
 }
 document.body.appendChild(component());
