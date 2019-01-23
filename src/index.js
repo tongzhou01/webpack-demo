d7b29cb58813a2dfa57f94c55a9a1c40634ad7d2
tongzhou <568309196@qq.com>
2019-01-23 21:53:04 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import image from './image.jpg';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' - ');
-    element.classList.add('hello');
-
-    // 将图像添加到我们现有的 div
-    var myIcon = new Image();
-    myIcon.src = image;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
     return element;
 }
 document.body.appendChild(component());
